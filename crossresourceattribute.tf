da31dcafa5c7709ba666723931a9476a897005e7
Adimanic123 <adityasharma0895@gmail.com>
2025-03-20 14:28:13 +0530

    Update crossresourceattribute.tf

--- crossresourceattribute.tf
+++ crossresourceattribute.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "allow_tls" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = "${aws_security_group.allow_tls.id}/32"
   cidr_ipv4         = aws_eip.lb.id
   from_port         = 443
   ip_protocol       = "tcp"
