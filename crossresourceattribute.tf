609cf9f859c3162acdb54b4ed8fc58a31825dc8c
Adimanic123 <adityasharma0895@gmail.com>
2025-03-20 17:59:31 +0530

    Update crossresourceattribute.tf

--- crossresourceattribute.tf
+++ crossresourceattribute.tf
@@ -2,14 +2,6 @@ resource "aws_eip" "lb" {
   domain   = "vpc"
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "${aws_eip.lb.public_ip}/32"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
+output "public_address" {
+  value = aws_eip.lb.public_ip
 }
