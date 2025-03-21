b0eeb947023113d8980c303e000fd3048c66df6f
Adimanic123 <adityasharma0895@gmail.com>
2025-03-21 13:34:54 +0530

    Update vartut.tf

--- vartut.tf
+++ vartut.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = var.sg_terraform
   cidr_ipv4         = var.vpn1
-  from_port         = var.tls
+  from_port         = var.tls_port
   ip_protocol       = "tcp"
   to_port           = var.tls_port
 }
