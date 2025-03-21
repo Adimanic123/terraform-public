77b4593188817b999d138e98f3ce7a849abba527
Adimanic123 <adityasharma0895@gmail.com>
2025-03-21 14:26:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,5 @@
-variable "sg_terraform" {
-  default = "sg-0880e1ada1d6f777e"
-}
+variable "sg_terraform" {}
 
-variable "vpn1" {
-  default = "167.103.2.204/32"
-}
+variable "vpn1" {}
 
-variable "tls_port" {
-  default = "443"
-}
+variable "tls_port" {}
