a278fbd489b8bfab4880924b05367109f27b69e4
Adimanic123 <adityasharma0895@gmail.com>
2025-03-21 13:25:31 +0530

    Create vartut.tf

---
+++ vartut.tf
@@ -0,0 +1,7 @@
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = var.sg_terraform
+  cidr_ipv4         = var.vpn1
+  from_port         = var.tls
+  ip_protocol       = "tcp"
+  to_port           = var.tls
+}
